c6a6bc6aed6fa405225633dfa6ec0867a705ec16
rezibos <rezibos>
2025-03-21 15:06:08 +0700

    c++

---
+++ Tugas_3.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class User {
+private:
+    string password;
+    string role;
+
+protected:
+    string email;
+
+public:
+    string username;
+
+    User(string uname, string pass, string mail) {
+        username = uname;
+        password = pass;
+        email = mail;
+        role = "User";
+        cout << "Akun '" << username << "' telah dibuat.\n";
+    }
+
+    string getRole() {
+        return role;
+    }
+
+    void setRole(string newRole) {
+        if (newRole == "Admin" || newRole == "User") {
+            role = newRole;
+            cout << "Role '" << username << "' diubah menjadi '" << newRole << "'.\n";
+        } else {
+            cout << "Role tidak valid!\n";
+        }
+    }
+
+    void login(string pass) {
+        if (pass == password) {
+            cout << "Login berhasil! Selamat datang, " << username << ".\n";
+        } else {
+            cout << "Login gagal! Password salah.\n";
+        }
+    }
+
+    ~User() {
+        cout << "Akun '" << username << "' telah dihapus.\n";
+    }
+};
+
+int main() {
+
+    User user1("User", "User", "User@gmail.com");
+
+    cout << "Username: " << user1.username << endl;
+    cout << "Role: " << user1.getRole() << endl;
+
+    user1.setRole("Admin");
+    user1.login("User");
+
+}
